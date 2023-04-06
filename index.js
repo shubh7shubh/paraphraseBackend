78d7c8917392216162a1ab0f8bfe67aea0d574e5
Deepak Aklecha <54493492+deepakaklecha@users.noreply.github.com>
2023-04-06 10:41:08 +0530

    updated index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 const path = require("path");
 const app = express();
+const helmet = require('helmet');
 app.use(express.json());
 const cors = require("cors");
 
@@ -12,10 +13,27 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-  res.setHeader("content-security-policy-report-only", "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io")
-  next();
-});
+// Set the CSP header using helmet middleware
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'"],
+      connectSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      mediaSrc: ["'self'"],
+      frameSrc: ["'self'", 'https://api.razorpay.com/']
+    },
+  })
+);
+
+// app.use(function(req, res, next) {
+//   res.setHeader("content-security-policy-report-only", "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io")
+//   next();
+// });
 
 
 //mongodb connection
