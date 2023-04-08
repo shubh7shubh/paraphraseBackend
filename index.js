142de6a74d3cb8a4a5ff30ff06c5de0bcd30ec38
Deepak Aklecha <54493492+deepakaklecha@users.noreply.github.com>
2023-04-08 09:58:29 +0530

    updated index

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ app.use(
     origin: "*",
   })
 );
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "connect-src 'self' https://lnkd.demdex.net https://media.licdn.com https://platform.linkedin.com https://static.licdn.com https://commention-backend.onrender.com");
-  next();
-});
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", 'example.com'],
+        objectSrc: ["'none'"],
+        frameSrc: ["'self'", 'blob:', '.licdn.com', '.linkedin.com', '.lynda.com', '.doubleclick.net', '.megaphone.fm', '.omny.fm', '.qualtrics.com', '.sounder.fm', 'lichat.azurewebsites.net', 'radar.cedexis.com', 'lnkd.demdex.net', 'cdn.embedly.com', 'www.facebook.com', 'embed.gettyimages.com', 'linkedin.github.io', 'www.linkedin-event.com', 'livestream.com', 'app.powerbi.com', 'msit.powerbi.com', 'w.soundcloud.com', 'embed.ted.com', 'player.vimeo.com', 'www.youtube.com', 'www.youtube-nocookie.com', 'https://silver-clafoutis-a44fda.netlify.app/'],
+        upgradeInsecureRequests: true
+    }
+}));
+
+app.use(helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true
+}));
+
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Security-Policy', "connect-src 'self' https://lnkd.demdex.net https://media.licdn.com https://platform.linkedin.com https://static.licdn.com https://commention-backend.onrender.com");
+//   next();
+// });
 
 
 //mongodb connection
